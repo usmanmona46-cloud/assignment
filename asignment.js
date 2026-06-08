c35b6384f618d006b53c9448ef45a01c1fd9b133
mona <usmanmona46@gmail.com>
2026-06-08 08:39:25 +0100

    Add files via upload

---
+++ asignment.js
@@ -0,0 +1,16 @@
+
+//To declare 2 variables, a and b, and assign values to them in JavaScript:
+
+let a = 10;
+let b = 5;
+
+
+//To declare two variables, a and b, and assign values to them in JavaScript, you can write:
+let a = 10;
+let b = 5;
+
+//Declare a variable minus that uses the - operator to store the result of subtracting the values stored in a and b
+
+
+
+
